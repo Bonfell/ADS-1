d1cbd79c680e6cbd3c7d3e6ea299c645ce0219b6
Bonfell <porkhunov99@inbox.ru>
2026-02-28 23:00:16 +0300

    Update alg.cpp
    dz

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,15 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
+#include <cmath>
+#include <cstdint>
 
 bool checkPrime(uint64_t value) {
-  if (n <= 1) return false;
-  if (n == 2) return true;
-  if (n % 2 == 0) return false;
-  for (int i = 3; i <= static_cast<int>(std::sqrt(n)); i += 2) {
-        if (n % i == 0) {
+  if (value <= 1) return false;
+  if (value == 2) return true;
+  if (value % 2 == 0) return false;
+  for (int i = 3; i <= static_cast<int>(std::sqrt(value)); i += 2) {
+        if (value % i == 0) {
             return false; 
         }
   }
@@ -119,7 +120,7 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
         } else if (p % 2 == 0) {
             isPPrime = false;
         } else {
-            uint64_t limit = static_cast<uint64_t>(sqrt(p));  
+            uint64_t limit = static_cast<uint64_t>(std::sqrt(p));  
             for (uint64_t i = 3; i <= limit; i += 2) {
                 if (p % i == 0) {
                     isPPrime = false;
@@ -130,7 +131,7 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
         if (q % 2 == 0) {
           isQPrime = false;  
         } else {
-            uint64_t limitQ = static_cast<uint64_t>(sqrt(q));
+            uint64_t limitQ = static_cast<uint64_t>(std::sqrt(q));
             for (uint64_t i = 3; i <= limitQ; i += 2) {
               if (q % i == 0) {
                 isQPrime = false;
